2c766d9401f6ab22bf3f7b5377ae0e3bb192731b
uo <schoolgirluche@gmail.com>
2019-04-28 19:30:09 -0700

    checkpoint 2594 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -21,4 +21,13 @@
         const previousSong = album.songs[previousSongIndex];
         player.playPause(previousSong);
       });
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+    }, 1000);
   }
